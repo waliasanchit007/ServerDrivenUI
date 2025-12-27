fdbf7b2f586685a481b49fb59dc96b4b6a210e92
Sanchit Walia <waliasanchit007@gmail.com>
2025-12-27 21:45:30 +0530

    Fix: Correct resource generated class imports in .shared package

--- composeApp/src/commonMain/kotlin/com/example/serverdrivenui/shared/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/serverdrivenui/shared/App.kt
@@ -16,8 +16,8 @@ import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import com.example.serverdrivenui.shared.generated.resources.Res
-import com.example.serverdrivenui.shared.generated.resources.compose_multiplatform
+import com.example.serverdrivenui.shared.Res
+import com.example.serverdrivenui.shared.compose_multiplatform
 
 @Composable
 @Preview
